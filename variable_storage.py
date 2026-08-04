5230689f227672911080b18259e0b34adf12c5e7
Aman Kumar <amankumarmaurya93@gmail.com>
2026-08-04 16:51:31 +0530

    variables storage

---
+++ variable_storage.py
@@ -0,0 +1,20 @@
+a = 5;
+b = 10;
+print(a);
+c = a;
+
+k= 9
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(k))
+
+name = "pirates"
+name1 = "pirates"
+print(id(name))
+print(id(name1))
+
+a = 'Hellow Welcome to tortuga'
+b = 'Hellow Welcome to tortuga'
+print(id(a))
+print(id(b))    
