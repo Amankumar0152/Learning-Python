9c4fd48479083fde3d9fb6b011bd5bba07ac5e46
Aman Kumar <amankumarmaurya93@gmail.com>
2026-08-10 09:11:12 +0530

    if else

---
+++ Conditions/if.py
@@ -0,0 +1,33 @@
+
+# num  = 5
+
+# if num % 2 == 0:
+#     print("even")
+
+# if num % 2 == 1:
+#     print("odd")
+
+# age = int(input("Enter Age: "))
+# if age >= 18:
+#     print("u can vote")
+# else:
+#     print("u can't vote")
+
+#else and debugging...
+
+# num = 7
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("odd")
+
+# print("dasvidaniya...")
+
+num = int(input("Enter a number: "))
+
+if num < 0:
+    print("Negative number")
+elif num > 0:
+    print("Positive number")
+else:
+    print("number is zero")
