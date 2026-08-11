c20b41b1a77ec4f7db75963de36948d0b2cb050b
Aman Kumar <amankumarmaurya93@gmail.com>
2026-08-11 19:59:15 +0530

    break and continue

--- Conditions/if.py
+++ Conditions/if.py
@@ -23,11 +23,24 @@
 
 # print("dasvidaniya...")
 
-num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
 
-if num < 0:
-    print("Negative number")
-elif num > 0:
-    print("Positive number")
+# if num < 0:
+#     print("Negative number")
+# elif num > 0:
+#     print("Positive number")
+# else:
+#     print("number is zero")
+
+num = int(input("enter a number to check even or odd: "))
+salary = 5
+
+if num % 2 == 0:
+    print("Even number")
+    if salary >= 5:
+        print("Great Job")
+    else:
+        print("Better luck next time")
+
 else:
-    print("number is zero")
+    print("odd number")
