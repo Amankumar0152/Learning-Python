a3bc3ea7ccf607a6dd7107111e1d6f21eb3bb2b4
Aman Kumar <amankumarmaurya93@gmail.com>
2026-08-11 19:42:37 +0530

    learning about while and lopp in cobra

---
+++ Conditions/for.py
@@ -0,0 +1,13 @@
+data = [2, 'Aman', 4.5, 8, 'Pirates', 'Spring Boot']
+
+for value in range(10):
+    print(value)
+
+
+#------------------------------
+# i = 0
+# n = len(data)
+# while i < n:
+#     print("Data: ",data[i])
+#     i+=1
+
---
+++ Conditions/while_loop.py
@@ -0,0 +1,23 @@
+# i = 1
+
+# while i <=10:
+#     print(52*i)
+#     i +=1
+#----------------------------
+# i = 1
+# j = 0
+# while i <=5:
+#     print("Hello ", end = "")
+
+#     while j <= 4:
+#         print("World!! ", end = "")
+#         j +=1
+
+#     i +=1
+#------------------------------=
+num = 1
+
+while num <= 10:
+    if num % 2 == 0:
+        print(num)
+    num += 1
